21e8851d09fcdcdd9ffaaffbe9b7c683f075c806
Cam Gallucci <cgallucci99@gmail.com>
2020-06-17 12:06:13 -0400

    finished todo list

--- TCS.js
+++ TCS.js
@@ -1,16 +1,14 @@
-var count = 0;
-
 function addTodo() {
     var text = document.getElementById("addToList").value;
     var todos = document.getElementById("todos").innerHTML;
-    document.getElementById("todos").innerHTML = todos + "<li id='count" + count + "'> <input class='boxes' type='checkbox'>" + text + "</li>";
-    count++;
+    document.getElementById("todos").innerHTML = todos + "<li class='list-group-item listItems'> <input class='boxes mr-3' type='checkbox'>" + text + "</li>";
+    document.getElementById("addToList").value = "";
 }
 
 function deleteList() {
-	var boxList = document.getElementsByClassName("boxes");
+	var boxList = document.getElementsByClassName("listItems");
 	for (var i = 0; i < boxList.length; i++) {
-		if (boxList[i].checked)
-			document.getElementById("count" + i).remove();
+		if (boxList[i].children[0].checked)
+			boxList[i].remove();
 	}
 }
