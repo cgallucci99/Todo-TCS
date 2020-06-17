1e3aca006857e4aac8bca50d4d2fb574c7bb4493
bhayes1998 <47541550+bhayes1998@users.noreply.github.com>
2020-06-17 11:42:55 -0400

    Added delete button

--- TCS.js
+++ TCS.js
@@ -1,5 +1,16 @@
+var count = 0;
+
 function addTodo() {
     var text = document.getElementById("addToList").value;
     var todos = document.getElementById("todos").innerHTML;
-    document.getElementById("todos").innerHTML = todos + "<li>" + text + "</li>";
+    document.getElementById("todos").innerHTML = todos + "<li id='count" + count + "'> <input class='boxes' type='checkbox'>" + text + "</li>";
+    count++;
+}
+
+function deleteList() {
+	var boxList = document.getElementsByClassName("boxes");
+	for (var i = 0; i < boxList.length; i++) {
+		if (boxList[i].checked)
+			document.getElementById("count" + i).remove();
+	}
 }
